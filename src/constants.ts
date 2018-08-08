7d2947bf7d63443a26ad582fbf0189f9a4bd0e11
rbhgn <robhgn@gmail.com>
2018-08-08 17:20:42 +0200

    Changed Constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
  export const DATABASE_URL = "postgres://rbhgn:97c06343321bcbe6b40f10dc9534e6e7@localhost:5454/portfolio_backend"
+//  export const DATABASE_URL = "postgres://xwfjwdkqgnboil:b91e069cc8ac363a86d7a2270728afaaf94ae1072ba1310252d07679cadf3d54@ec2-107-21-98-165.compute-1.amazonaws.com:5432/d9h7h5j36jjies"
 
 
 //  USER = rbhgn
